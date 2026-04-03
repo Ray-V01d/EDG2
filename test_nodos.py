4cbd8066efa1e9ca2c53d889efea2d9a56180a91
Ray <evilmantus@gmail.com>
2026-04-02 21:45:49 -0400

    style: corrige la indentación en la creación de nodos

--- test_nodos.py
+++ test_nodos.py
@@ -17,10 +17,10 @@ if __name__ == "__main__":
     nodo4 = NodoListaSimplementeEnlazada(20)
     nodo5 = NodoListaSimplementeEnlazada(25)
 
-nodo1.sig = nodo2
-nodo1.sig.sig = nodo3
-nodo1.sig.sig.sig = nodo4
-nodo1.sig.sig.sig.sig = nodo5
+    nodo1.sig = nodo2
+    nodo1.sig.sig = nodo3
+    nodo1.sig.sig.sig = nodo4
+    nodo1.sig.sig.sig.sig = nodo5
 
 # Creación de una lista Python de nodos (NO RECOMENDADO)
 
