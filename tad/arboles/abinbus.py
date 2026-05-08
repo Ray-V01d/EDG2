197f3a2a898d3f051fcde2e3ffe20bc12a9cba35
Ray_V01d <evilmantus@gmail.com>
2026-05-08 15:23:20 -0400

    fix: corregir importaciones y manejo de excepciones en ArbolBinarioBusqueda

--- tad/arboles/abinbus.py
+++ tad/arboles/abinbus.py
@@ -1,5 +1,6 @@
-from tad.listas.abin import ArbolBinario as ABin
-from tad.arboles.nodos import NodoArbolBinario 
+from tad.arboles.abin import ArbolBinario as ABin
+from tad.arboles.nodos import NodoArbolBinario as NAB
+from tad.arboles.excepciones import DuplicatedKeyError
 
 
 class ArbolBinarioBusqueda(ABin):
@@ -11,11 +12,11 @@ class ArbolBinarioBusqueda(ABin):
 
     def __agregar(self, sub_arbol, nueva_clave):
         if sub_arbol is None:
-            sub_arbol = NodoArbolBinario(nueva_clave)
+            sub_arbol = NAB(nueva_clave)
         elif nueva_clave < sub_arbol.clave:
             sub_arbol.izq = self.__agregar(sub_arbol.izq, nueva_clave)
         elif nueva_clave > sub_arbol.clave:
             sub_arbol.der = self.__agregar(sub_arbol.der, nueva_clave)
         else:
-            raise ValueError(f"La clave {nueva_clave} ya existe en el árbol.")
+            raise DuplicatedKeyError(f"{nueva_clave}")
         return sub_arbol
